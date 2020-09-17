4f74f7fdc388a8ddb0315fa2ca2494e531bcce68
Jordan Kay <jordan@fleuronic.com>
2020-09-17 13:36:35 -0400

    Mark functions as public for use in SPM

--- Sources/CasePaths-Extensions/CasePaths_Extensions.swift
+++ Sources/CasePaths-Extensions/CasePaths_Extensions.swift
@@ -1,17 +1,17 @@
 import CasePaths
 
 extension CasePath {
-    func matches(_ other: Root) -> Bool {
+    public func matches(_ other: Root) -> Bool {
         return extract(from: other) != nil
     }
 }
 
 extension CasePath {
-    func map<R>(_ root: Root, _ f: (Value) -> (R)) -> R? {
+    public func map<R>(_ root: Root, _ f: (Value) -> (R)) -> R? {
         extract(from: root).map(f)
     }
     
-    func mutate(_ root: Root, _ f: (Value) -> (Value)) -> Root? {
+    public func mutate(_ root: Root, _ f: (Value) -> (Value)) -> Root? {
         guard let newValue = extract(from: root).map(f) else {
             return nil
         }
